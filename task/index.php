c389341c372ff6fc75e50b1f4cec6dcee5b8fc1c
ruubje03 <rubenoosting2003@gmail.com>
2026-03-13 13:44:17 +0100

    ticket 1.1/1.2 af

---
+++ task/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title></title>
+    <?php require_once 'head.php'; ?>
+</head>
+
+<body>
+    <header>
+        <a href="create.php"
+    </header>
+    
+    <div class="container">
+
+
+    </div>
+
+</body>
+
+</html>
