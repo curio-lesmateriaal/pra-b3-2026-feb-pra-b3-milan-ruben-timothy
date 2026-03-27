9ce29354d7cdaf9c8f51dc4001feac386bb8af1a
Hannes <rollemandufour@gmail.com>
2026-03-27 12:04:20 +0100

    updated create.php en de css daarvan

--- tasks/create.php
+++ tasks/create.php
@@ -6,40 +6,46 @@
     <?php require_once '../head.php'; ?>
 </head>
 
-<body>
-    <form action="../backend/taskController.php" method="POST">
-    <input type="hidden" name="action" value="insert">
-     <div class="form-group">
-        <label for="titel">title</label>
-        <input type="text" name="titel">
-    </div>
+<body class="tasks_index_body">
+    <div class="container-create">
+        <h1>Nieuw taak aanmaken</h1>
+        <form action="../backend/taskController.php" method="POST">
+            <input type="hidden" name="action" value="insert">
+            <div class="form-group">
+                <label class="label-create" for="titel">title</label>
+                <input type="text" name="titel">
+            </div>
 
-     <label for="afdeling">Afdeling:</label>
-      <select id="afdeling" name="afdeling" required>
-        <option value="">-- Kies afdeling --</option>
-        <option value="personeel">Personeel</option>
-        <option value="horeca">Horeca</option>
-        <option value="techniek">Techniek</option>
-        <option value="inkoop">Inkoop</option>
-        <option value="klantenservice">Klantenservice</option>
-        <option value="groen">Groen</option>
-      </select>
+            <label class="label-create" for="afdeling">Afdeling:</label>
+            <select id="afdeling" name="afdeling" required>
+                <option value="">-- Kies afdeling --</option>
+                <option value="personeel">Personeel</option>
+                <option value="horeca">Horeca</option>
+                <option value="techniek">Techniek</option>
+                <option value="inkoop">Inkoop</option>
+                <option value="klantenservice">Klantenservice</option>
+                <option value="groen">Groen</option>
+            </select>
 
-     <div class="form-group">
-        <label for="beschrijving">description</label>
-        <textarea name="beschrijving"></textarea>
-    </div>
+            <div class="form-group">
+                <label class="label-create" for="beschrijving">description</label>
+                <textarea name="beschrijving"></textarea>
+            </div>
 
-     <div class="form-group">
-        <label for="deadline">deadline</label>
-        <input type="date" name="deadline">
+            <div class="form-group">
+                <label class="label-create" for="deadline">deadline</label>
+                <input type="date" name="deadline">
+            </div>
+
+            <div class="form-group">
+                
+                <button class="submit_btn" type="submit">Opslaan</button>
+
+                <a class="discard_btn" href="index.php" class="cancel">Annuleren</a>
+            </div>    
+        </form>
     </div>
     
-    <div class="form-group">
-        <input type="submit" value="submit">
-        <a class="discard_btn" href="index.php" class="cancel">Annuleren</a>
-    </div>
-    </form>
 
 </body>
 </html>
